e87fd2df15782770d9861153284d26aeaf420102
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-31 22:36:41 +0200

    Implementation cont.
    - main
    - home
    - routes

--- CovalentQuickStart/src/CovalentQuickStart.Client/src/main.ts
+++ CovalentQuickStart/src/CovalentQuickStart.Client/src/main.ts
@@ -4,6 +4,7 @@ import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { MdIconRegistry } from '@angular2-material/icon';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent, environment } from './app/';
+import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { TD_MEDIA_PROVIDERS } from '@covalent/core';
 
 if (environment.production) {
@@ -15,5 +16,6 @@ bootstrap(AppComponent, [
   provideForms(),
   MdIconRegistry,
   HTTP_PROVIDERS,
+  APP_ROUTER_PROVIDERS,
   TD_MEDIA_PROVIDERS,
 ]);
--- CovalentQuickStart/src/CovalentQuickStart.Client/src/system-config.ts
+++ CovalentQuickStart/src/CovalentQuickStart.Client/src/system-config.ts
@@ -41,6 +41,8 @@ const barrels: string[] = [
   'app',
   'app/shared',
   'services',
+  'app/main',
+  'app/home',
   /** @cli-barrel */
 ];
 
