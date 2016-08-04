2edd69ba265323d4c5d64d808c10b729947f4e63
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-08-04 23:47:08 +0200

    Tests fixes

--- CovalentQuickStart/src/CovalentQuickStart.Client/src/app/dashboard/dashboard.component.spec.ts
+++ CovalentQuickStart/src/CovalentQuickStart.Client/src/app/dashboard/dashboard.component.spec.ts
@@ -2,12 +2,16 @@
 
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import { addProviders, async, inject } from '@angular/core/testing';
-import { DashboardComponent } from './dashboard.component';
+import { addProviders, beforeEachProviders, async, inject, describe, expect, it } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { ItemsService } from '../shared';
+import { UsersService } from '../users';
+import { } from './';
 
 describe('Component: Dashboard', () => {
-  it('should create an instance', () => {
-    let component = new DashboardComponent();
+  beforeEachProviders(() => [ItemsService, UsersService]);
+  it('should create an instance', inject([ItemsService, UsersService], (itemsService, usersService) => {
+    let component = new DashboardComponent(itemsService, usersService);
     expect(component).toBeTruthy();
-  });
+  }));
 });
--- CovalentQuickStart/src/CovalentQuickStart.Client/src/app/main/main.component.spec.ts
+++ CovalentQuickStart/src/CovalentQuickStart.Client/src/app/main/main.component.spec.ts
@@ -1,13 +1,15 @@
 /* tslint:disable:no-unused-variable */
-
+import { Router } from '@angular/router';
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import { addProviders, async, inject, describe, it, expect } from '@angular/core/testing';
+import { addProviders, async, inject, describe, it, expect, beforeEachProviders } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 
 describe('Component: Main', () => {
-  it('should create an instance', () => {
-    let component = new MainComponent();
+
+  beforeEachProviders(() => [Router]);
+  it('should create an instance', inject([Router], (router: Router) => {
+    let component = new MainComponent(router);
     expect(component).toBeTruthy();
-  });
+  }));
 });
